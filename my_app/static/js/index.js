f1ad95cebd632787dae331222be0fb70212ed609
john4650-hub <johndelvin51@gmail.com>
2024-01-29 00:35:51 +0300

    Made timer work in background and now we  are done think

--- my_app/static/js/index.js
+++ my_app/static/js/index.js
@@ -1,22 +1,57 @@
 import Phaser from "../lib/phaser.js";
 import { ProgArc } from "./progArc.js";
 
+let dropDownList = document.createElement('select');
+let DATE = new Date()
+
+let [curDate, curMonth, curYear] = DATE.toLocaleString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }).split(' ')
+
+let opts;
+for (let i = 1; i <= 10; i++) {
+  opts = document.createElement('option')
+  opts.innerText = i * 30
+  opts.setAttribute('value', i * 30)
+  dropDownList.appendChild(opts)
+}
+document.getElementById('droplist').appendChild(dropDownList)
+
+
 class progress_bar extends Phaser.Scene {
   constructor() {
     super({ key: "progress_bar" })
   }
-
   create() {
     let x_pos = 90
     let y_pos = 100
     let rad_sz = 150
-    let tim_ = 3
     let circle = this.add.circle(x_pos, y_pos, rad_sz).setOrigin(0)
     circle.setStrokeStyle(5, 0xffffff, 1);
-    let arc = new ProgArc(this, x_pos, y_pos, rad_sz, -90, 268, false, tim_)
-    // let tim = new TimerText(this,x_pos,y_pos,tim_,{})
-    //, endAngle, anticlockwise, fillColor);
-    // var arc
+    let arc = new ProgArc(this, x_pos, y_pos, rad_sz, -90, 268, false)
+
+    document.getElementById('startBtn').addEventListener('click', function() {
+      let timeTaken = dropDownList.value
+      arc.intiEndAngle()
+      let foo = new EventSource(`/stream/${timeTaken}`)
+      foo.onmessage = function(event) {
+        if (event.data == 'done') {
+          foo.close(); // Close the connection when the timer is done
+          fetch('/saveTimeSpent', {
+            method: 'POST',
+            headers: {
+              'content-type': "application/json"
+            },
+            body: JSON.stringify({
+              "curMonth": curMonth,
+              "curDate": curDate,
+              "curYear": curYear,
+              "timeTaken": timeTaken
+            })
+          })
+        } else {
+          arc.upDateTimer(event.data, timeTaken); // Update the timer
+        }
+      };
+    })
   }
 }
 
@@ -31,18 +66,13 @@ class Streak extends Phaser.Scene {
 
 var config = {
   type: Phaser.AUTO,
-  // parent: 'progress',
+  parent: 'app',
   width: 440,
-  height: 640,
+  height: 440,
   scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    mode: Phaser.Scale.FIT
   },
 
   scene: [progress_bar]
 };
 var game = new Phaser.Game(config);
-// game.events.off('hidden',game.onHidden,game)
-// game.events.off('visible',game.onVisible,game)
-// game.events.off('blur',game.onBlur,game)
-// game.events.off('focus',game.onFocus,game)
--- my_app/streaker/views.py
+++ my_app/streaker/views.py
@@ -1,31 +1,39 @@
-from flask import render_template, request
+from flask import render_template, request, Response
 from my_app import streaker_app
 import json
-
+import time
 @streaker_app.route('/')
 @streaker_app.route('/streaker')
 def index():
   return render_template('index.html')
 
-@streaker_app.route('/get-streak-data', methods=['GET'])
+@streaker_app.route('/get-streak-data')
 def send_json():
   with open('Db/data.json','r') as file:
     data = json.load(file)
   return json.dumps(data)
 
-# @streaker_app.route('/get-video-hist', methods=['POST'])
-# def save_data():
-#   data = request.json
-#   unknown_key=list(data.keys())[0]
-#   with open('Db/watch_hist.json','r') as fhand:
-#     vidData=json.load(fhand)
-#     vidData[unknown_key]=data[unknown_key]
-#   with open('Db/watch_hist.json','w') as fhand:
-#     json.dump(vidData,fhand)
-#   return jsonify({"message": "Data saved successfully!"})
-
-# @streaker_app.route('/set-video-hist', methods=['GET'])
-# def set_vdata():
-#   with open('Db/watch_hist.json','r') as fhand: 
-#     data=json.load(fhand)
-#   return json.dumps(data)
+@streaker_app.route('/stream/<int:duration>')
+def stream(duration):
+    def generate():
+        remaining_duration = duration * 60 # Convert minutes to seconds
+        while remaining_duration > 0:
+            time.sleep(1) # Wait for 1 second
+            mins, secs = divmod(remaining_duration, 60) # Calculate minutes and seconds
+            yield f'data: {str(mins).zfill(2)}:{str(secs).zfill(2)}\n\n' # Yield formatted time
+            remaining_duration -= 1 # Decrement timer
+        yield f'data: done\n\n' # Yield a signal to indicate the timer is done
+    return Response(generate(), mimetype='text/event-stream')
+    
+@streaker_app.route('/saveTimeSpent',methods=['POST'])
+def saveTimeSpent():
+  progData=request.get_json()
+  cur_month = progData["curMonth"]
+  cur_year = progData["curYear"]
+  with open('Db/data.json','r') as fh:
+    data = json.load(fh)
+    fh.close()
+  data[cur_year][cur_month][int(progData['curDate'])-1]+=int(progData["timeTaken"])
+  with open('Db/data.json','w') as fh:
+    json.dump(data,fh)
+  return "data saved to database"
